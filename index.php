da29e0f180a7bcc7214cd573cecb63a94b031290
Connor Smyth <connorsmyth@rogers.com>
2020-11-23 16:16:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-/**
- * Plugin Name:       Art & Science SEO
- * Plugin URI:        https://bitbucket.org/artscience/as-ai-image-alt-tags
- * Description:       Art & Science SEO plugin for SEO related tasks
- * Version:           0.1.0
- * Requires at least: 4.9
- * Requires PHP:      7.2
- * Author:            Art & Science
- * Author URI:        https://artscience.ca
- * Text Domain:       as-seo
- * Domain Path:       /languages
- *
- * @package           ASAltText
- */
+
